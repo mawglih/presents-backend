2cfaa18b58b52cb0466c788e9afec2703e082504
Oleg Markoff <mawglih@gmail.com>
2018-12-04 22:02:34 -0500

    cors update

--- server.js
+++ server.js
@@ -2,12 +2,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const cors = require('cors');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const podarki = require('./routes/api/podarki');
 
 const app = express();
+
+//cors
+const allowedOrigins = [
+  'http://localhost:3000',
+];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //Body parser
 app.use(bodyParser.urlencoded({extended: false}));
